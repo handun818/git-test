c5af0929196bffa3c26c04b7c576e7b5d4f9bfa2
hangbanPC <44085000@qq.com>
2020-08-31 15:30:43 +0800

    把本地工程添加到git

---
+++ src/img/sdasd/com/FirstGit.java
@@ -0,0 +1,11 @@
+package img.sdasd.com;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("heelo-git-project");
+        System.out.println("heelo-git-project");
+        System.out.println("heelo-git-project");
+        System.out.println("heelo-git-project");
+        System.out.println("heelo-git-project");
+    }
+}
