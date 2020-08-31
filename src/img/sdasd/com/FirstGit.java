2ccaba2d8788974f0290fb00a12c4eb89fefb6b9
hangbanPC <44085000@qq.com>
2020-08-31 16:12:59 +0800

    修改firstgit

--- src/img/sdasd/com/FirstGit.java
+++ src/img/sdasd/com/FirstGit.java
@@ -7,11 +7,13 @@ public class FirstGit {
         System.out.println("heelo-git-project");
         System.out.println("heelo-git-project");
         System.out.println("heelo-git-project");
-
+//hello.test
 
         }
     public void test(){
         int i=0;
         i+=10;
+
+        System.out.println(i);
     }
 }
